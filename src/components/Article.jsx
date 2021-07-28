7cf4f70364f7d9c69a0c5a78786599a30bea5059
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-28 16:21:15 -0700

    Article.jsx laid out

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Article() {
+export default function Article({ title, author, description }) {
   return (
-    <div>
+    <>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <p>{description}</p>
             
-    </div>
+    </>
   );
 }
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export default function Search({ search, onChange, onSubmit }) {
   return (
     <form>
-      <input id="search" type="text" value={search} name="search" onChange={onChange} onSubmit={onSubmit} /> 
+      <input placeholder="Search" id="search" type="text" value={search} name="search" onChange={onChange} onSubmit={onSubmit} /> 
       <button>Search</button> 
     </form>
   );
